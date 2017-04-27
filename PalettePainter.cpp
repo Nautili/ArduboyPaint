22f541cf201d8c25d208d1d5bb2441ad92802991
nautili <lear.vg@gmail.com>
2017-04-27 17:33:39 -0500

    Appearance tweak.

--- PalettePainter.cpp
+++ PalettePainter.cpp
@@ -12,11 +12,11 @@ void PalettePainter::paint() {
   
   int curBrushIndex = palette.getCurrentBrushIndex();
   for(int i = max(0, curBrushIndex - 3); i < min(curBrushIndex + 4, palette.getNumBrushes()); ++i) {
-    int brushHeight = 27 - (curBrushIndex - i) * 9;
+    int brushHeight = 28 - (curBrushIndex - i) * 9;
     palette.getBrush(i)->paintIcon(WIDTH - 11, brushHeight);
   }
 
-  arduboy.drawRect(WIDTH-2, 27, 2, 8, WHITE);
+  arduboy.drawRect(WIDTH-2, 28, 2, 8, WHITE);
 }
 
 int PalettePainter::getPaletteWidth() {
